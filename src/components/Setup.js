f060397723084da33a1b7a2bc6293150032bd227
Michael Harris <mhguitar1997@gmail.com>
2019-07-01 16:54:37 -0700

    Setup component displys names

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css'
-import {Dropdown} from 'semantic-ui-react'
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Dropdown } from "semantic-ui-react";
 
 class Setup extends Component {
-    state = {
-        names: ''
-    }
+  state = {
+    names: []
+  };
 
-componentDidMount(){
-    this.props.weekOne.map(emp => {
-        console.log(emp)
-    })
+  componentDidMount() {
+    this.setState({
+      names: this.props.weekOne.map(emp => {
+          return {
+              ...emp,
+              text: emp.name //need text value for semantic ui to display names
+          }
+      })
+    });
+  }
+  render() {
+    return <div><Dropdown placeholder="Select your name" search selection options={this.state.names}/></div>;
+  }
 }
-    render() {
-        return (
-            <div>
-                setup
-            </div>
-        );
-    }
-}
-
 
 export default Setup;
