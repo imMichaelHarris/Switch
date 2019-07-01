b5bc081b11d0d2fc4d792092bbc6155df5ec720f
Michael Harris <mhguitar1997@gmail.com>
2019-07-01 15:57:49 -0700

    Create and import weekOne and weekTwo schedules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import {weekOne} from './weekOne'
+import {weekTwo} from './weekTwo'
 
 function App() {
+  //
   return (
     <div className="App">
-
+      
     </div>
   );
 }
