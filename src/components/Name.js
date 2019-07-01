96725fdc4339df2256a867b4c37dafb0f7e299a1
Michael Harris <mhguitar1997@gmail.com>
2019-07-01 16:09:33 -0700

    Setup file create

--- src/components/Name.js
+++ src/components/Name.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Name = () => {
     return (
         <div>
-            
+            test
         </div>
     );
 };
---
+++ src/components/Setup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Setup extends Component {
+    state = {}
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default Setup;
