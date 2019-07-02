0c9dd389c57faa0ed1822ddc337fe4ffb44a9aa5
Michael Harris <mhguitar1997@gmail.com>
2019-07-01 17:05:26 -0700

    Install node sass package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import {weekOne} from './weekOne'
 import {weekTwo} from './weekTwo'
 import Setup from './components/Setup';
--- src/components/Setup.js
+++ src/components/Setup.js
@@ -12,7 +12,7 @@ class Setup extends Component {
       names: this.props.weekOne.map(emp => {
           return {
               ...emp,
-              value: emp.name,
+              value: emp.name, //need value to select
               text: emp.name //need text value for semantic ui to display names
           }
       })
