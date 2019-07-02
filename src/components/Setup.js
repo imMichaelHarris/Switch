4585c9d624c3dbce694a1605fdd62157b883aa85
Michael Harris <mhguitar1997@gmail.com>
2019-07-01 17:27:32 -0700

    Add switch type prop on state and changeHandler

--- src/components/Setup.js
+++ src/components/Setup.js
@@ -1,25 +1,45 @@
 import React, { Component } from "react";
 import "semantic-ui-css/semantic.min.css";
-import { Dropdown } from "semantic-ui-react";
+import { Dropdown, Button } from "semantic-ui-react";
 
 class Setup extends Component {
   state = {
-    names: []
+    names: [],
+    switchType: null
   };
 
   componentDidMount() {
     this.setState({
       names: this.props.weekOne.map(emp => {
-          return {
-              ...emp,
-              value: emp.name, //need value to select
-              text: emp.name //need text value for semantic ui to display names
-          }
+        return {
+          ...emp,
+          value: emp.name, //need value to select
+          text: emp.name //need text value for semantic ui to display names
+        };
       })
     });
   }
+  switchType = (type) => {
+      this.setState({
+          switchType: type
+      })
+  }
   render() {
-    return <div><Dropdown placeholder="Select your name" search selection options={this.state.names}/></div>;
+    return (
+      <div>
+        <Dropdown
+          placeholder="Select your name"
+          search
+          selection
+          options={this.state.names}
+        />
+        <Button.Group size="large">
+          <Button onClick={}>Day Off</Button>
+          <Button.Or />
+          <Button>Time Change</Button>
+        </Button.Group>
+      </div>
+    );
   }
 }
 
