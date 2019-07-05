4a33a48f60af8825ab8b72a3f538750a351bb5b3
Michael Harris <mhguitar1997@gmail.com>
2019-07-04 23:16:39 -0700

    Create Employee component

---
+++ src/components/Employee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Employee = (props) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Employee;
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
 
 
 const EmployeeList = (props) => {
     return (
         <div>
+            <Link to="/">Refine your search</Link>
 List
         </div>
     );
