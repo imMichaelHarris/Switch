993b5fbb48cbaef44a6b9f58c01c0cf459ef248d
Michael Harris <mhguitar1997@gmail.com>
2019-07-05 16:59:22 -0700

    Flip card function create

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
-import ReactCardFlip from 'react-card-flip'
+import ReactCardFlip from "react-card-flip";
 
-const Employee = props => {
-    const {name} = props.employee
-  return (
-    <Card>
-      <Card.Content>
-        <Card.Header>{name}</Card.Header>
-        <Card.Description>
-        {props.employee[props.day].substring(14,22)}
-        </Card.Description>
-        <Card.Meta>{props.employee[props.day].substring(0,14) }</Card.Meta> 
-      </Card.Content>
-    </Card>
-  );
-};
+class Employee extends React.Component {
+  state = {
+    isFlipped: false
+  };
+
+  flipCard = () => {
+    this.setState(prevState => {
+      return {
+        isFlipped: !prevState.isFlipped
+      };
+    });
+  };
+
+  render() {
+    const { name } = this.props.employee;
+    return (
+      <Card>
+        <Card.Content>
+          <Card.Header>{name}</Card.Header>
+          <Card.Description>
+            {this.props.employee[this.props.day].substring(14, 22)}
+          </Card.Description>
+          <Card.Meta>
+            {this.props.employee[this.props.day].substring(0, 14)}
+          </Card.Meta>
+        </Card.Content>
+      </Card>
+    );
+  }
+}
 
 export default Employee;
