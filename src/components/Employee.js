e8e2302535a8ce3aee21823ea14368abdeda644c
Michael Harris <mhguitar1997@gmail.com>
2019-07-04 23:20:26 -0700

    Now renders employee names

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const Employee = (props) => {
+    console.log(props)
     return (
         <div>
-            
+            <h3>{props.employee.name}</h3>
         </div>
     );
 };
--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Employee from './Employee';
 
 
 const EmployeeList = (props) => {
     return (
         <div>
             <Link to="/">Refine your search</Link>
-List
+{props.availablePeople.map(emp => <Employee employee={emp} key={emp.name} />)}
         </div>
     );
 };
