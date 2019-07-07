959fc492c8439d76992f14e80ef4c9a60e499622
Michael Harris <mhguitar1997@gmail.com>
2019-07-06 21:06:23 -0700

    Add https to website link

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = (props) => {
         daySearch={props.daySearch}
       />
 
-      <p style={{position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)', width: '100%', fontSize: '1rem'}}>Switch app v0.0.2 by <a href="immichaelharris.com">Michael Harris</a></p>
+      <p style={{position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)', width: '100%', fontSize: '1rem'}}>Switch app v0.0.2 by <a href="https://immichaelharris.com">Michael Harris</a></p>
     </div>
   );
 };
