cc64904afe175fc73e230300d71b85be31ec96b6
Michael Harris <mhguitar1997@gmail.com>
2019-07-17 20:01:20 -0700

    Update schedule snapshot text date

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ const Home = (props) => {
         daySearch={props.daySearch}
       />
   <p style={{marginTop: "20px",fontSize: ".8rem"}}>Work in progress</p>
-  <p style={{marginTop: "20px",fontSize: ".8rem"}}>Schedule snapshot taken at 7/12/19 11:43PM PST</p>
+  <p style={{marginTop: "20px",fontSize: ".8rem"}}>Schedule snapshot taken at 7/17/19 8:00PM PST</p>
 
       <p style={{position: 'absolute', bottom: '0', left: '50%', transform: 'translateX(-50%)', width: '100%', fontSize: '1rem'}}>Switch app v0.2.4 by <a href="https://immichaelharris.com">Michael Harris</a></p>
     </div>
