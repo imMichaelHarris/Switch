e9f232bcff81254ebd177d864af27e0e5c0f8acb
imMichaelHarris <34087309+imMichaelHarris@users.noreply.github.com>
2019-09-01 10:39:22 -0700

    Update times

--- src/components/DateandTime.js
+++ src/components/DateandTime.js
@@ -41,7 +41,7 @@ class DateandTime extends React.Component {
           <Datepicker
             autoComplete="off"
             placeholderText="I want to be off by..."
-            minDate={moment().add(1, "days").toDate()}
+            minDate={new Date()}
             maxDate={moment()
               .add(14, "days")
               .toDate()}
@@ -64,7 +64,7 @@ class DateandTime extends React.Component {
             autoComplete="off"
             name="date"
             placeholderText="Select a date"
-            minDate={moment().add(1, "days").toDate()}
+            minDate={new Date()}
             maxDate={moment()
               .add(14, "days")
               .toDate()}
@@ -77,7 +77,7 @@ class DateandTime extends React.Component {
             autoComplete="off"
             name="giveUpDay"
             placeholderText="Give up day"
-            minDate={moment().add(1, "days").toDate()}
+            minDate={new Date()}
             maxDate={moment()
               .add(14, "days")
               .toDate()}
