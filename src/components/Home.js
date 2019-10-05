4cae2ef85996e473bc6b256f9b401f63063ea517
Michael Harris <mhguitar1997@gmail.com>
2019-10-05 09:08:57 -0700

    Update schedules

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ const Home = props => {
       />
       <p style={{ marginTop: "20px", fontSize: ".8rem" }}>Very much a work in progress</p>
       <p style={{ marginTop: "20px", fontSize: ".8rem" }}>
-        Schedule snapshot taken at 9/21/19 8:48am PST
+        Schedule snapshot taken at 10/5/19 9:06am PST
       </p>
 
       <p
@@ -27,7 +27,7 @@ const Home = props => {
           fontSize: "1rem"
         }}
       >
-        Switch app v0.5.5 by{" "}
+        Switch app v0.5.6 by{" "}
         <a href="https://immichaelharris.com">Michael Harris</a>
       </p>
     </div>
