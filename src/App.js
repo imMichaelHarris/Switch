676a1607b6b589a75026f2153de687a8a7382b7d
Michael Harris <mhguitar1997@gmail.com>
2019-10-13 09:56:58 -0700

    Updatew schedules and times

--- src/App.js
+++ src/App.js
@@ -113,7 +113,7 @@ class App extends React.Component {
     this.props.history.push("/results");
   };
   changeDate = e => {
-    const date = moment(e).isAfter("Oct 13 2019")
+    const date = moment(e).isAfter("Oct 27 2019")
       ? `sec${moment(e).format("dddd")}`
       : moment(e).format("dddd");
     this.state.switchType === "Time Change"
